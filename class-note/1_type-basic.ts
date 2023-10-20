3a7c23a02b57e03cda79f6ad4dba83dfadd92d32
wendy <wendy@gridwiz.com>
2023-10-20 09:06:40 +0900

    변수와 함수 타입 정의하기

---
+++ class-note/1_type-basic.ts
@@ -0,0 +1,26 @@
+//js 문자열 선언
+// var str = 'hello';
+
+
+//TS 변수
+let str: string = 'hello';
+let num: number = 10;
+
+//TS 배열
+let arr: Array<number> = [1, 2, 3];
+let arr2: number[] = [1, 2, 3];
+let arr3: Array<string> = [];
+
+//TS 튜플
+//배열 각 인덱스에 타입을 정해줄 수 있음
+let address: [string, number] = ['gangnam', 10];
+
+//TS 객체
+let obj: object = {};
+let person: {name: string, age: number} = {
+    name: 'hello',
+    age: 100,
+}
+
+// TS boolean
+let show: boolean = true;
